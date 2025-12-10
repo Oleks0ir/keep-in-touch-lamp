2523665bba90eb60e11b5e104a863426eefd2548
Alex0ir <139131088+Oleks0ir@users.noreply.github.com>
2025-12-10 20:12:43 +0100

    The Blink base created. Basic filesystem dependencies made. All works

---
+++ Mushrooms/keep-in-touch-lamp/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); // toggle the LED state
+  delay(1000);
+}
